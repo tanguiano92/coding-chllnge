bc91a8eff5120605f046925dc4ddb5a63790bdc2
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-22 16:58:14 -0700

    update

--- sketch.js
+++ sketch.js
@@ -2,18 +2,14 @@ let button;
 
 
 function setup() {
-  createCanvas(400, 400);
+
 
   button = createButton("click to change color");
 
-  image(img, 0, 0);
 
 
 }
 
 function draw() {
-  background(220);
-}
-
-
+  
 }
