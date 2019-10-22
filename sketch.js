7615f9121a4324ec5c07c757b61f4bd18d93db8e
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-22 15:50:16 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let button;
+
 function setup() {
   createCanvas(400, 400);
+
+  createCanvas(100, 100);
+   background(0);
+   button = createButton('click me');
+   button.position(19, 19);
+   button.mousePressed(changeBG);
+
 }
 
 function draw() {
   background(220);
+}
+
+function changeBG() {
+  let val = random(255);
+  background(val);
 }
