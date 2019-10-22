9689ce0ff023a4642abe66dcab91e399894a90ce
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-22 15:52:23 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,6 @@
 let button;
 
 function setup() {
-  createCanvas(400, 400);
-
-  createCanvas(100, 100);
-   background(0);
    button = createButton('click me');
    button.position(19, 19);
    button.mousePressed(changeBG);
