3da1eb475d5c6037351604e98628197e82aa9c88
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-22 17:15:20 -0700

    update

--- sketch.js
+++ sketch.js
@@ -5,11 +5,27 @@ function setup() {
 
 
   button = createButton("click to change color");
-
-
-
+  button.mousePressed(buttonIsPressed);
 }
 
 function draw() {
-  
+
+  if (buttonIsPressed) {
+    if (button === LEFT) {
+      ellipse(50, 50, 50, 50);
+    }
+    if (button === RIGHT) {
+      rect(25, 25, 50, 50);
+    }
+    if (button === CENTER) {
+      triangle(23, 75, 50, 20, 78, 75);
+    }
+  }
+
+}
+
+function buttonIsPressed(){
+
+
+
 }
